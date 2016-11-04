3b24b864f27c3bb80c0a4f484e6012eff78778b3
jhall0310 <jonathanphall@me.com>
2016-11-04 16:52:05 -0700

    race winner determined

--- main.js
+++ main.js
@@ -1,37 +1,18 @@
+console.log('linked!');
 
-$(document).ready(function() {
- console.log('JQuery is connected')
+$(window).keypress(function(e){
+    if(e.keyCode === 108) {
+        $('.player1').css({'margin-left': '+=100'});
+        checkWinner();
+} else if (e.keyCode === 97) {
+        $('.player2').css({'margin-left': '+=100'});
+        checkWinner();
+    }
+});
 
-//  $('#track').click(function() {
-//        $('.player1').animate({
-//        'marginLeft' : "+=100px" //moves right
-//        });
-// });
-
-// $('#track').keyup(function() {
-//       $('.player2').animate({
-//       'marginLeft' : "+=100px" //moves right
-//       });
-// });
-$(document).keydown(function (e) {
-        var keyCode = e.keyCode || e.which,
-        arrow = {left: 37, up: 38, right: 39, down: 40 };
-
-        switch (keyCode) {
-
-            case arrow.left:
-                $('.player1').animate({
-                'marginLeft' : "+=100px" //moves right
-                });
-            break;
-
-            case arrow.right:
-                $('.player2').animate({
-                'marginLeft' : "+=100px" //moves right
-                });
-            break;
-        }
-    });
-
-
-})
+function checkWinner(){
+    if ($('.player1').css('margin-left')==='1400px') {
+        alert('red wins!');
+    } else if ($('.player2').css('margin-left')==='1400px')
+    alert('green wins!');
+}
