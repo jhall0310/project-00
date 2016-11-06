8b96c53b784275768be0807fecfbee883cfa6dcf
jhall0310 <jonathanphall@me.com>
2016-11-06 01:56:06 -0700

    appendeed winning events:

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ $(window).keypress(function(e){
 function checkWinner1(){
     if ($('.player1').css('margin-left')==='1400px') {
       $('.container-fluid').append('<img  src="http://www.dailyhaha.com/_pics/super_ugly_monkey.jpg" />');
+      $('h1').css('color', 'white');
         alert('Jean wins!');
 
         $('body').css('background-color', '#5122EA');
