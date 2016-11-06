dcbb749e3f08aaa1b618f83be6adfef48c1f5774
jhall0310 <jonathanphall@me.com>
2016-11-06 01:15:32 -0700

    decloration on winner changed

--- main.js
+++ main.js
@@ -6,20 +6,34 @@ $(document).ready(function() {
 $(window).keypress(function(e){
     if(e.keyCode === 108) {
         $('.player1').css({'margin-left': '+=50'});
-        checkWinner();
+        checkWinner1();
+        checkWinner2();
 } else if (e.keyCode === 97) {
         $('.player2').css({'margin-left': '+=50'});
-        checkWinner();
+        checkWinner1();
+        checkWinner2();
     }
 
 });
 
-function checkWinner(){
+function checkWinner1(){
     if ($('.player1').css('margin-left')==='1400px') {
+      $('.container-fluid').append('<img  src="http://www.dailyhaha.com/_pics/super_ugly_monkey.jpg" />');
         alert('Jean wins!');
-        $('.container-fluid').append('<img  src="http://i.imgur.com/8xv5A8D.png" />');
-    } else if ($('.player2').css('margin-left')==='1400px')
-    alert('Justin wins!');
+
+        $('body').css('background-color', '#5122EA');
+
+    }
+}
+
+function checkWinner2(){
+    if ($('.player2').css('margin-left')==='1400px') {
+      $('.container-fluid').append('<img  src="http://s3.amazonaws.com/hiphopdx-production/2013/07/12-2chainz.jpg" />');
+        alert('Justin wins!');
+
+        $('body').css('background-color', '#BBF97E');
+
+    }
 }
 
 $('button').click(function() {
