964d281bd69a027914e4d88f31a106d2fecdff18
jhall0310 <jonathanphall@me.com>
2016-11-05 22:34:02 -0700

    reset button fixed, had to add document to JSS

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 console.log('linked!');
 
-$('button').click(function() {
-      location.reload();
-      console.log('button clicked');
-  });
+$(document).ready(function() {
 
 
 $(window).keypress(function(e){
     if(e.keyCode === 108) {
-        $('.player1').css({'margin-left': '+=100'});
+        $('.player1').css({'margin-left': '+=50'});
         checkWinner();
 } else if (e.keyCode === 97) {
-        $('.player2').css({'margin-left': '+=100'});
+        $('.player2').css({'margin-left': '+=50'});
         checkWinner();
     }
 
@@ -20,6 +17,14 @@ $(window).keypress(function(e){
 function checkWinner(){
     if ($('.player1').css('margin-left')==='1400px') {
         alert('Jean wins!');
+        $('.container-fluid').append('<img  src="http://i.imgur.com/8xv5A8D.png" />');
     } else if ($('.player2').css('margin-left')==='1400px')
     alert('Justin wins!');
 }
+
+$('button').click(function() {
+      location.reload();
+      console.log('button clicked');
+  });
+
+})
